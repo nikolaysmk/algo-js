900ba3af42fae6a7a52f7b3418038fdbbdf1818c
Nikolay Smk <nikolaysmyk@icloud.com>
2023-05-16 10:15:35 +0300

    Initial commit
    Create 2637-Promise-Time-Limit.js
    
    Create 2621-Sleep.js
    
    Create 2619-Array-Prototype-Last.js

---
+++ LeetCode/2621-Sleep.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    // Возвращаем новый Promise и вызываем resolve через millis миллисекунд
+    return new Promise((reslolve) => setTimeout(reslolve, millis))
+}
+
+/**
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
