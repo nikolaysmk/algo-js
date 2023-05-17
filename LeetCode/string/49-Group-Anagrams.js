64c20cc55366650700d8a176514c74b8cade4417
Nikolay Smk <nikolaysmyk@icloud.com>
2023-05-17 11:03:54 +0300

    Create 49-Group-Anagrams.js

---
+++ LeetCode/string/49-Group-Anagrams.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    const map = new Map()
+    for (let str of strs) {
+        const key = [...str].sort().join('')
+        if (!map.has(key)) map.set(key, [])
+        map.get(key).push(str)
+    }
+    return [...map.values()]
+}
